1c74dcdbd4f0bb4523e7070fa145ba6cf15c7909
victorMedeiros82 <victor.medeiros82@gail.com>
2025-04-10 21:44:20 -0300

    Atualização da animação de arco, e adcionando sprite do arco e flexa.

---
+++ Assets/GameFolder/Kreeper/EnemyAttack.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class KeeperAttackCollider : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.GetComponent<Character>().life--;
+        }
+    }
+
+
+}
